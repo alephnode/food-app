80e92fc84b4c875fdd1f29a3dcc32e57a599bb67
Stephen Ward <srmward@gmail.com>
2017-02-19 21:45:18 -0800

    added styling, routes for different food choices! finished app, will enhance but this is template

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 const RESTAURANT_COUNT = 7;
+const HOME_COUNT = 3;
 var Pool = require('pg').Pool;
 
 
@@ -18,16 +19,35 @@ process.on('unhandledRejection', function(e) {
 var pool = new Pool(CONFIG)
 
 app.set('view engine', 'pug')
-app.get('/', function (req, res) {
+
+.get('/', function(req, res){
+  res.render('index')
+})
+
+.get('/grab-something', function (req, res) {
   var random = Math.floor((Math.random() * RESTAURANT_COUNT));
   pool
     .query('select * from restaurant_info')
     .then(function(result) {
-      res.render('index', { message: result.rows[random].name })
-    });
+      var name = result.rows[random].name;
+      var address = result.rows[random].address;
+      var phone = result.rows[random].phone;
+      var hours = result.rows[random].hours;
 
+      res.render('app-grab', { name: name, address: address, phone: phone, hours: hours })
+    });
+})
+.get('/make-something', function (req, res) {
+  var random = Math.floor((Math.random() * HOME_COUNT));
+  pool
+    .query('select * from home_dinners')
+    .then(function(result) {
+      var name = result.rows[random].name;
+
+      res.render('app-make', { name: name })
+    });
 })
 
-app.listen(3000, function () {
+.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
